0b4d5be757bdbef423276d68d1e8cddedb4d9d74
ZhongGang <120859274@qq.com>
2013-07-16 22:34:13 +0800

    before advice of aop

---
+++ src/test/java/com/icode/aop/SimpleBeforeAdvice.java
@@ -0,0 +1,36 @@
+package com.icode.aop;
+
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.framework.ProxyFactory;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-16
+ * Time: 下午10:29
+ */
+public class SimpleBeforeAdvice implements MethodBeforeAdvice {
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        System.out.println("This is a before advice~~~~");
+    }
+
+
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker();
+
+        SimpleBeforeAdvice beforeAdvice = new SimpleBeforeAdvice();
+
+        ProxyFactory proxyFactory = new ProxyFactory(speaker);
+        proxyFactory.addAdvice(beforeAdvice);
+
+        Speaker proxySpeaker = (Speaker) proxyFactory.getProxy();
+
+        speaker.speak();
+
+        proxySpeaker.speak();
+    }
+}
---
+++ src/test/java/com/icode/aop/Speaker.java
@@ -0,0 +1,14 @@
+package com.icode.aop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-16
+ * Time: 下午10:27
+ */
+public class Speaker {
+
+    public void speak() {
+        System.out.println("Hello World!");
+    }
+}
