cb020b62e6f7dee8d18db65b8f0cc728b532e741
ZhongGang <120859274@qq.com>
2013-07-16 11:37:47 +0800

    struts integration with testng test framework

---
+++ src/test/java/com/icode/web/dto/ShopDTOTest.java
@@ -0,0 +1,18 @@
+package com.icode.web.dto;
+
+import org.apache.struts2.StrutsTestCase;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-16
+ * Time: 上午11:27
+ */
+public class ShopDTOTest extends StrutsTestCase {
+
+    @Test
+    public void testName() throws Exception {
+        System.out.println("Hello world~~~~~");
+    }
+}
