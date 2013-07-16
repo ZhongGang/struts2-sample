97f621215b363f2d294a789cd8b70f5b1dd9dce7
ZhongGang <120859274@qq.com>
2013-07-17 00:52:13 +0800

    annotation of aop

---
+++ src/test/java/com/icode/aop/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.icode.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-17
+ * Time: 上午12:39
+ */
+@Aspect
+public class LoggingAspect {
+
+    @Around(value = "execution(* com.icode.aop..*.*(..))")
+    public Object log(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Before");
+        Object result = pjp.proceed();
+        System.out.println("After");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:testApplicationContext.xml");
+        Speaker speaker = (Speaker) applicationContext.getBean("speaker");
+        speaker.speak();
+    }
+}
